c9c223a2833c6acf1a2f876e32c7e2d0a796b6d7
nadya.sakharova <nadezhda.sakharova@fogstream.ru>
2021-04-12 21:23:49 +1000

    (newsList) сверстаны экраны новостей и просмотра новостей

--- app/navigation/RootStackNavigator.tsx
+++ app/navigation/RootStackNavigator.tsx
@@ -2,10 +2,21 @@ import React, { ReactElement } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { RootStackParamList } from "./types";
 import LaunchScreen from "../launchScreen/containers/LaunchScreen";
+import NewsList from "../news/containers/NewsList";
+import CurrentNews from "../currentNews/containers/CurrentNews";
+import { headerStyles } from "./styles";
 
 const Stack = createStackNavigator<RootStackParamList>();
 
 const RootStackNavigator = (): ReactElement => {
+  const currentNewsOptions = {
+    headerStyle: headerStyles.headerStyle,
+    headerTitleStyle: headerStyles.headerTitleBackStyle,
+    title: '',
+    // headerBackImage: BackButton,
+    headerBackTitleVisible: false
+  };
+
   return (
     <Stack.Navigator initialRouteName="LaunchScreen">
       <Stack.Screen
@@ -13,6 +24,16 @@ const RootStackNavigator = (): ReactElement => {
         component={LaunchScreen}
         options={{headerShown: false}}
         />
+      <Stack.Screen
+        name="NewsList"
+        component={NewsList}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="CurrentNews"
+        component={CurrentNews}
+        options={currentNewsOptions}
+      />
     </Stack.Navigator>
   )
 };
